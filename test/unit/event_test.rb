b13a8be3ad47b8b4f27ae6bd3041d2f3605bb559
Ryan Walker <ryanwalker@dhcp-10-85-6-198.linh.uc.edu>
2008-11-25 21:24:16 -0500

    test

--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -6,5 +6,15 @@ class EventTest < Test::Unit::TestCase
     event = Event.new
     assert event.valid?
   end
+  
+  def test_must_have_date
+    event = Event.new(:date => 1.days.ago)
+    assert event.valid?
+  end
+
+  # def test_must_have_title
+  #   event = Event.new(:title => 'Newbie night')
+  #   assert event.valid?
+  # end
 
 end
