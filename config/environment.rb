0867cc64b002b530b8bf16314b3180e5e38d2de5
Rob Biedenharn <Rob@AgileConsultingLLC.com>
2009-02-10 19:03:00 -0500

    shoulda

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
 
+  config.gem 'thoughtbot-shoulda', :version => '>=2.0.6', :lib => 'shoulda', :source => 'http://gems.github.com'
+
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
