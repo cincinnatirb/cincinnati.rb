bcfe8a60e54f052f28c7a452380fcfc583d3b303
Rob Biedenharn <Rob@AgileConsultingLLC.com>
2009-03-25 14:37:07 -0400

    Event#to_s, Location#to_s and fixtures to get started

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,8 @@ class Event < ActiveRecord::Base
 
   belongs_to :location
   validates_presence_of :start_time, :location_id
-  
+
+  def to_s
+    "#{self.date.to_date} #{self.start_time.to_s(:hhmm_p)} - #{self.topic} (#{self.location.name})"
+  end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,7 @@ class Location < ActiveRecord::Base
 
   validates_presence_of :name, :address
 
+  def to_s
+    "#{self.id}: #{self.name}; #{self.address}"
+  end
 end
