09811771c3d122a98ee5a823c605e650ef0c741d
Ryan Walker <wwwguru@gmail.com>
2009-06-16 19:28:17 -0400

    added voter

---
+++ app/models/voter.rb
@@ -0,0 +1,25 @@
+class Voter
+require 'mechanize'
+
+  PEOPLE = [
+    ['Ryan','Walker','ry@anotherventure.com'],
+    ['Michael','Guterl','mguterl.com'],
+    ]
+
+  def self.execute
+    a = WWW::Mechanize.new { |agent|
+      agent.user_agent_alias = 'Mac Safari'
+    }
+
+    a.get('http://cincinnatiinnovates.com/contest/vote_form/25') do |page|
+      PEOPLE.each do |person|
+        vote_result = page.form_with(:name => 'vote_form') do |form|
+          form['vote[first_name]'] = person[0]
+          form['vote[last_name]'] = person[1]
+          form['vote[email_confirmation]'] = form['vote[email]'] = person[2]
+        end.submit
+      end
+    end
+  end
+
+end
