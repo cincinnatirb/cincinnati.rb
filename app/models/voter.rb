c3461f03c863064cb436ed866b9c9ca79562313d
Rob Biedenharn <Rob@AgileConsultingLLC.com>
2009-06-16 19:57:08 -0400

    add Rob

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,10 +1,11 @@
-class Voter
 require 'mechanize'
 
+class Voter
   PEOPLE = [
-    ['Ryan','Walker','ry@anotherventure.com'],
-    ['Michael','Guterl','mguterl.com'],
-    ]
+            ['Ryan','Walker','ry@anotherventure.com'],
+            ['Michael','Guterl','mguterl@gmail..com'],
+            %w[ Rob Biedenharn Rob@AgileConsultingLLC.com ],
+           ]
 
   def self.execute
     a = WWW::Mechanize.new { |agent|
@@ -14,10 +15,14 @@ require 'mechanize'
     a.get('http://cincinnatiinnovates.com/contest/vote_form/25') do |page|
       PEOPLE.each do |person|
         vote_result = page.form_with(:name => 'vote_form') do |form|
-          form['vote[first_name]'] = person[0]
-          form['vote[last_name]'] = person[1]
-          form['vote[email_confirmation]'] = form['vote[email]'] = person[2]
+          form['vote[first_name]'], form['vote[last_name]'], form['vote[email]'] = person
+          form['vote[email_confirmation]'] = form['vote[email]']
         end.submit
+        puts "vote_result is a #{vote_result.class.name}"
+        puts vote_result.body
+        if vote_result.search('//h2[@class="alreadyVoted"]')
+          puts "%s %s <%s> has already voted"%[*person]
+        end
       end
     end
   end
