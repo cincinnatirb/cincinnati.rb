46edfc1b9b74f3b06cd4d1131a06a5ac5ca72a16
Bill Barnett <bbarnett@maggie.local>
2009-06-16 20:35:35 -0400

    added addresses

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -3,9 +3,12 @@ require 'mechanize'
 class Voter
   PEOPLE = [
             %w[ Ryan Walker ry@anotherventure.com ],
+            %w[ Bill Barnett bill@budgetsketch.com ],
             %w[ Michael Guterl mguterl@gmail.com ],
             %w[ Rene Barnett rene.barnett@gmail.com ],
+            %w[ Sandy Lea scripsafe@gmail.com ],
             %w[ Rob Biedenharn Rob@AgileConsultingLLC.com ],
+            %w[ Polly Barnett polly@polly-barnett.com ],
             %w[ James Smith st23am@gmail.com ],
            ]
 
